ea97292fcb0dc60a76a777242b00ab3a02518eec
Ömer Nalçacı <nalcaciomer@outlook.com>
2022-03-24 14:57:02 +0300

    Setting up Swagger 2 for Spring REST API

--- Hrms/src/main/java/kodlamaio/Hrms/HrmsApplication.java
+++ Hrms/src/main/java/kodlamaio/Hrms/HrmsApplication.java
@@ -2,6 +2,12 @@ package kodlamaio.Hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -10,4 +16,12 @@ public class HrmsApplication {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
 
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 }
